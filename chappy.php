43c0832abcb33d0751d062c9a6d94ecb9a51cf7e
Aleksander Hansson <aleksander.hansson@me.com>
2013-08-27 22:30:18 +0200

    69.2

--- chappy.php
+++ chappy.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: Chappy, The Cheap Web App
-Description: Turn Your PageLines Site Into an iPhone Web App!  Over 12 Different Options, Customize the the Image Added to the iPhone Homescrean, Customize Everything About the Pop Up, & Advance Behavior Control.
-Version: 69.1
+Description: Turn Your PageLines Site Into an iPhone Web App! Over 12 Different Options, Customize the the Image Added to the iPhone Homescrean, Customize Everything About the Pop Up & Advance Behavior Control.
+Version: 69.2
 Author: Aleksander Hansson & Mike Zielonka
 Author URI: http://chappy.ahansson.com/developers/
 Plugin URI: http://chappy.ahansson.com/
